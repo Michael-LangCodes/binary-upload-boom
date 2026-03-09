0720adba3975054cea320e27b410146db1d41f48
MichaelCodes <michael.lang3303@gmail.com>
2026-03-08 19:13:59 -0500

    config comments

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
+//connecting to the DataBase
 const connectDB = async () => {
   try {
+    //connect to the database using the connection string
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -11,6 +13,7 @@ const connectDB = async () => {
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
+    //log the error if there are any
     console.error(err);
     process.exit(1);
   }
--- config/passport.js
+++ config/passport.js
@@ -5,26 +5,33 @@ const User = require("../models/User");
 module.exports = function (passport) {
   passport.use(
     new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      //finds the data about the email
       User.findOne({ email: email.toLowerCase() }, (err, user) => {
         if (err) {
           return done(err);
         }
+        //chcks if there is no user 
         if (!user) {
           return done(null, false, { msg: `Email ${email} not found.` });
         }
+        //checks if there is no password
         if (!user.password) {
           return done(null, false, {
             msg:
               "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
           });
         }
+        //checks if the password is correct
         user.comparePassword(password, (err, isMatch) => {
+          //checks if there is an error
           if (err) {
             return done(err);
           }
+          //checks if it is a match
           if (isMatch) {
             return done(null, user);
           }
+          //if not error or match it notifies of invalid inputs
           return done(null, false, { msg: "Invalid email or password." });
         });
       });
